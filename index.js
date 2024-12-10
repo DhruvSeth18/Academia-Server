44e160e74025639e6182212824041ee22e6c98b9
Dhruv_Seth <dhruvseth090704@gmail.com>
2024-12-10 15:13:29 +0530

    some chages with the cors

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ app.use(helmet());
 app.use(
     cors({
         origin: ["https://academia-front-end.vercel.app","http://localhost:3000", "http://localhost:5173"],
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
+        credentials: true
     })
 );
 app.use(bodyParser.json());
