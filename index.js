94eb0933b16d2cdc396743e286c1ffa9904fda86
Dhruv_Seth <dhruvseth090704@gmail.com>
2024-12-10 14:52:54 +0530

    some imp changes -by ds

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ app.use(
     cors({
         origin: "https://academia-front-end.vercel.app",
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
     })
 );
 app.use(bodyParser.json());
